7fdbad4e6e634057899e10e419d084434b4c2e2a
Zackary Santana <ZackZackYack@gmail.com>
2020-06-16 22:34:49 -0400

    Added basic styling and background for game

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+// Libraries
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
+// Styles
+import './styles.scss';
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const GameBox = styled.div`
+    width: 80vw;
+    height: 80vh;
+    z-index: 1;
+
+    background-color: black;
+    border: 2px gray solid;
+`;
 
 export default function RPG() {
     return (
-        <h1>Test</h1>
+        <Fragment>
+            <div id="background"></div>
+            <div id="middleground"></div>
+            <div id="foreground"></div>
+            <Background>
+                <GameBox>
+                    {/* Do game lol */}
+                </GameBox>
+            </Background>
+        </Fragment>
     );
 }
